83d34ae86f456649450e20072099109845ed2fe1
Abhishek A Pillai <abhishekap2005@gmail.com>
2025-11-22 16:24:39 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+## Odoo_Hackathon
+
+Problem Statement : StockMaster
+
+Team Details : Mohammed Hussain Shaikh, Abhishek A Pillai, Prashant Mahadev Goundadkar, P. Anurag
+
+Reviewer Name : Aman Patel (ampa)
---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+=======
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+>>>>>>> 729b037669324b8117b8163984c78562ba01a81f
